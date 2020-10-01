84364e94c478df3fc486767d2eb87f9ab7586aa7
taif <taifbakshi.dev@gmail.com>
2020-10-01 23:25:28 +0600

    Updates

---
+++ precache-manifest.dd08be919515b08137e8e224a233e520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfbde8268d573e5e47971e0eb2a4f14",
+    "url": "/Taif71/mobodex.git/index.html"
+  },
+  {
+    "revision": "d26883dc845e079ea882",
+    "url": "/Taif71/mobodex.git/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/Taif71/mobodex.git/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "d26883dc845e079ea882",
+    "url": "/Taif71/mobodex.git/static/js/main.e15fface.chunk.js"
+  },
+  {
+    "revision": "14394166ad449aa4f823",
+    "url": "/Taif71/mobodex.git/static/js/runtime~main.0a0281b5.js"
+  }
+]);
